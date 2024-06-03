a7042f50504165ecf24002f2647f0bdf4b33deb2
Michael Noser <michael@noser.moe>
2024-06-03 12:26:00 +0200

    Do not set gateway address

--- openvpn/protonvpn/update-port.sh
+++ openvpn/protonvpn/update-port.sh
@@ -17,7 +17,7 @@ function box_out() {
 }
 
 open_port() {
-    natpmpc -a 1 0 udp 60 -g 10.2.0.1 && natpmpc -a 1 0 tcp 60 -g 10.2.0.1
+    natpmpc -a 1 0 udp 60 && natpmpc -a 1 0 tcp 60
 }
 
 remote() {
